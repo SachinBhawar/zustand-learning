e9b0076217e9e4ca5414db2220f287dd7c0886d2
Sachin Bhawar <sachinbhavar@gmail.com>
2025-10-21 10:47:29 +0530

    Changes in counter app

--- zustand setup/src/App.jsx
+++ zustand setup/src/App.jsx
@@ -1,7 +1,38 @@
-import "./App.css";
+import useCounterStore from "./store/counterStore";
 
 function App() {
-    return <div>Hello world</div>;
+    const { count, increment, decrement, reset } = useCounterStore();
+
+    return (
+        <div className="flex flex-col items-center justify-center ">
+            <h2 className="m-12 text-2xl font-bold text-grey-500">{count}</h2>
+            <div className="flex flex-row item-center justify-center">
+                <button className="px-3 py-2 bg-green-300 rounded-md mr-4" onClick={increment}>
+                    Increment
+                </button>
+                <button className="px-3 py-2 bg-red-300 rounded-md mr-4" onClick={decrement}>
+                    Decrement
+                </button>
+                <button className="px-3 py-2 bg-blue-300 rounded-md" onClick={reset}>
+                    Reset
+                </button>
+            </div>
+        </div>
+    );
 }
+
+const styles = {
+    container: {
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial, sans-serif",
+    },
+    buttons: {
+        display: "flex",
+        justifyContent: "center",
+        gap: "10px",
+        marginTop: "20px",
+    },
+};
 
 export default App;
--- zustand setup/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const store = create((set) => ({
-    count:0,
-    incCount: (set)=>{}
-}));
